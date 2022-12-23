0dc97304112a071dee01da54725fc4c0e19054f9
cvenero <cvenero@unsa.edu.pe>
2022-12-23 14:29:20 +0000

    extablecemos las expresiones regulares para filtrar la informacion

--- view.pl
+++ view.pl
@@ -3,68 +3,34 @@ use strict;
 use warnings;
 use CGI;
 use DBI;
-#REGISTER
+
 my $q = CGI->new;
 print $q->header('text/xml;charset=UTF-8');
+#view.pl
+my $owner = $q->param('owner');
+my $title = $q->param('title');
 
-my $user = $q->param('user');
-my $password = $q->param('password');
-my $first = $q->param('first');
-my $last = $q->param('last');
- 
-  my $sql = "SELECT * FROM User WHERE userName=?";
-  if($user){
-    my @row = checkLogin($user,$password, $first, $last);
-    print "<?xml version='1.0' encoding='utf-8'?>
-    <user>
-      <owner>$user</owner>
-      <firstName>$first</firstName>
-      <lastName>$last</lastName>
-    </user>";
-  }else{
-  print "<?xml version='1.0' encoding='utf-8'?>
-  <user>
-  </user>";
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+
+  my $sth1 = $dbh->prepare("SELECT owner,text FROM Articles WHERE owner=? AND title=?");
+  $sth1->execute($owner,$title); 
+  my @row = $sth1->fetchrow_array;
+
+  if($row[1] =~ /^\#{1}(\s.*)/){
+    print "<h1>$1</h1>";
   }
-sub checkLogin{
-  my $userQuery = $_[0];
-  my $passwordQuery = $_[1];
-  my $firstQuery = $_[2];
-  my $lastQuery = $_[3];
+  elsif($row[1] =~ /^\#{6}(\s.*)/){
+    print "<h6>$1</h6>";
+  }
+  elsif($row[1] =~ /^(\*{2})(\w.*)(\*{2})$/){
+    print "<p><strong>$2</strong></p>";
+  }
 
-  my $user = 'alumno';
-  my $password = 'pweb1';
-  my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
-  my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
-  
-  my $sql = "INSERT INTO Users VALUES (?,?,?,?)";
-  #my $sql = "SELECT * FROM Users WHERE userName=? AND password=?";
-  my $sth = $dbh->prepare($sql);
-  $sth->execute($userQuery, $passwordQuery, $firstQuery, $lastQuery);
-  my @row = $sth->fetchrow_array;
-  # print "<?xml version='1.0' encoding='utf-8'?>
-  # <user>
-  # <owner>$row[0]</owner>
-   # <firstName>$row[3]</firstName>
-   # <lastName>$row[2]</lastName>
-   # </user>";
-  $sth->finish;
+  $sth1->finish;
+
   $dbh->disconnect;
-  return @row;
-}
-#sub successLogin{
-#print "<?xml version='1.0' encoding='utf-8'?>
-# <user>
-# <owner>$row[0]</owner>
-# <firstName>$row[3]</firstName>
-# <lastName>$row[2]</lastName>
-# </user>"; 
-#}
-#sub showLogin{
-# my $body = << "XML";
-# <?xml version='1.0' encoding='utf-8'>
-# <user>
-# </user>
-#XML
-# return $body;
-#}
+  
+
