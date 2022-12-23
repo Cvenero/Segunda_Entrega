0061e13ca25b4158910bb443f45f28f1ec3363fc
cvenero <cvenero@unsa.edu.pe>
2022-12-23 15:43:13 +0000

    hacemos la consulta con owner y un titulo para luego retornar su texto correspondiente

--- article.pl
+++ article.pl
@@ -6,61 +6,31 @@ use DBI;
 
 my $q = CGI->new;
 print $q->header('text/xml;charset=UTF-8');
-
-my $title = $q->param('title');
+#view.pl
 my $owner = $q->param('owner');
-my $text = $q->param('text');
+my $title = $q->param('title');
 
-  my $user = 'alumno';
-  my $password = 'pweb1';
-  my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
-  my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
-  if(defined($title) and defined($text)){
-  my $sql = "INSERT INTO Articles VALUES (?,?,?)";
-  my $sth = $dbh->prepare($sql);
-  $sth->execute($title, $owner, $text);
-  $sth->finish;
-  ############################################
-   my $sth1 = $dbh->prepare("SELECT title, text, owner FROM Articles WHERE owner=?");
-  $sth1->execute($owner); 
-  # my @row = $sth1->fetchrow_array;
-  # print "$row[2]\n";
+my $sth1 = $dbh->prepare("SELECT owner, title, text FROM Articles WHERE owner=? AND title=?");
+  $sth1->execute($owner,$title); 
+my @row = $sth1->fetchrow_array;
   
-  print "
-    <?xml version='1.0' encoding='utf-8'?>
+if($owner = $row[0] and $title = $row[1]){
+   print "<?xml version='1.0' encoding='utf-8'?>
     <article>
-      <title>$title</title>
-      <text>$text</text>
-    </article>
-    ";
-  $sth1->finish;
-  } 
-  else{
-  print "
-    <?xml version='1.0' encoding='utf-8'?>
-    <article>
-    </article>
-    ";
+     <owner>$row[0]</owner>
+     <title>$row[1]</title>
+     <text>$row[2]</text>
+    </article>";
+}else{
+   print "<?xml version='1.0' encoding='utf-8'?>
+  <article>
+  </article>";
+}
 
-  }
-
-  # my $sth1 = $dbh->prepare("SELECT title, text FROM Articles WHERE owner=?");
-  #$sth1->execute($owner); 
-
-  #while(my @row = $sth1->fetchrow_array){
-  # print "@row\n";
-  
-  #print "
-  # <?xml version='1.0' encoding='utf-8'?>
-  # <article>
-  #   <title>$row[0] </title>
-  #   <text>$row[1]</text>
-  # </article>
-  # ";
-  #}  
-  #$sth1->finish;
-  
-  $dbh->disconnect;
-  
-
+$sth1->finish;
+$dbh->disconnect;
